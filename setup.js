6ed38c05576e24d4deaf31392882b09c2ffbc6bf
ReactiveSlime <33995597+ReactiveSlime@users.noreply.github.com>
2023-04-21 21:32:17 +1000

    see commit 9f4332e
    Forgot to include the updated setup.js and leaked my Spotify creds

--- setup.js
+++ setup.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const http = require('http');
 const url = require('url');
 const SpotifyWebApi = require('spotify-web-api-node');
 const port = 3000;
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: 'a22aadec759141548e2b46f38e3e1fcd',
-    clientSecret: '16f3e168e716461d9551996f66a41de1',
-    redirectUri: 'http://localhost:3000/callback' // Replace this with your own redirect URI
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: 'http://localhost:3000/callback' // Replace this with your own redirect URI
 });
 
 http.createServer((req, res) => {
@@ -15,26 +16,29 @@ http.createServer((req, res) => {
   if (requestUrl.pathname === '/callback') {
     const code = requestUrl.query.code;
     spotifyApi.authorizationCodeGrant(code)
-      .then(data => {
-        console.log('Access token:', data.body['access_token']);
-        console.log('Refresh token:', data.body['refresh_token']);
-        console.log('Expires in:', data.body['expires_in']);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(`<html><body><p>Authorization code received. Check console for tokens.</p></body></html>`);
-		setTimeout(() => process.exit(), 2000);
+      .then((data) => {
+        const { access_token, refresh_token, expires_in } = data.body;
+        console.log(`Access token: ${access_token}`);
+        console.log(`Refresh token: ${refresh_token}`);
+        console.log(`Expires in: ${expires_in}`);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>Authorization code received. Check console for tokens.</p></body></html>');
         res.end();
-		
       })
-      .catch(err => {
-        console.log('Error getting authorization code:', err);
-        res.writeHead(500, {'Content-Type': 'text/html'});
-        res.write(`<html><body><p>Error getting authorization code. Check console for details.</p></body></html>`);
+      .catch((err) => {
+        console.log(`Error getting authorization code: ${err}`);
+        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>Error getting authorization code. Check console for details.</p></body></html>');
         res.end();
       });
   } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(`<html><body><a href="${spotifyApi.createAuthorizeURL(['user-read-playback-state'], 'state')}">Authorize with Spotify</a></body></html>`);
     res.end();
   }
-}).listen(3000);
-console.log('To authorize with spotift go to: http://localhost:3000')
+}).on('error', (err) => {
+  console.error(`Server error: ${err}`);
+}).listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+  console.log(`To authorize with Spotify, go to: http://localhost:${port}`);
+});
